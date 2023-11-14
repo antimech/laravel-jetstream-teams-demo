4813dcf35792cb810d8c5f9d10c9dd786aa1bc73
Artur Gauzer <artur.gauzer@pm.me>
2023-11-14 23:44:31 +0600

    Update Laravel

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -1,34 +1,36 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Fortify\Features;
 use Laravel\Jetstream\Jetstream;
-use Tests\TestCase;
 
-class RegistrationTest extends TestCase
-{
-    use RefreshDatabase;
+test('registration screen can be rendered', function () {
+    $response = $this->get('/register');
 
-    public function test_registration_screen_can_be_rendered()
-    {
-        $response = $this->get('/register');
+    $response->assertStatus(200);
+})->skip(function () {
+    return ! Features::enabled(Features::registration());
+}, 'Registration support is not enabled.');
 
-        $response->assertStatus(200);
-    }
+test('registration screen cannot be rendered if support is disabled', function () {
+    $response = $this->get('/register');
 
-    public function test_new_users_can_register()
-    {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
-        ]);
+    $response->assertStatus(404);
+})->skip(function () {
+    return Features::enabled(Features::registration());
+}, 'Registration support is enabled.');
 
-        $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
-    }
-}
+test('new users can register', function () {
+    $response = $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+        'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(RouteServiceProvider::HOME);
+})->skip(function () {
+    return ! Features::enabled(Features::registration());
+}, 'Registration support is not enabled.');
